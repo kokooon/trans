ff97d42be72aac3e6f4f688d7a6237bf9914f4e7
Gaetan Marzullo <gmarzull@k0r4p1.42mulhouse.fr>
2023-11-21 14:57:35 +0100

    21_Nov_2

---
+++ frontend/src/component/Navbar.tsx
@@ -0,0 +1,16 @@
+// Navbar.tsx
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Accueil</li>
+        <li>À propos</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
