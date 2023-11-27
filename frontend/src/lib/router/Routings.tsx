ebdfd8aab8a2635b0f1bce8947825bb5d0cc565d
Gaetan Marzullo <gmarzull@k0r4p1.42mulhouse.fr>
2023-11-27 13:55:59 +0100

    27_Nov

--- frontend/src/lib/router/Routings.tsx
+++ frontend/src/lib/router/Routings.tsx
@@ -1,3 +1,4 @@
+//Routings.tsx
 /**
  * @note
  * for hook alternative of route element composition:
@@ -8,34 +9,71 @@
  * - https://reactrouter.com/docs/en/v6/upgrading/v5#note-on-link-to-values
  */
 
+// import { Suspense } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+// import RequireAuth from '@/lib/components/auth/RequireAuth';
+// import Page404 from '@/lib/pages/404';
+
+// import { routes, privateRoutes } from './routes';
+
+// const Routings = () => {
+//   return (
+//     <Suspense>
+//       <Routes>
+//         {routes.map((routeProps) => (
+//           <Route {...routeProps} key={routeProps.path as string} />
+//         ))}
+//         {privateRoutes.map(({ element, ...privateRouteProps }) => (
+//           <Route
+//             element={
+//               <RequireAuth
+//                 redirectTo={`/login?redirectTo=${privateRouteProps.path}`}
+//               >
+//                 {element}
+//               </RequireAuth>
+//             }
+//             {...privateRouteProps}
+//             key={`privateRoute-${privateRouteProps.path}`}
+//           />
+//         ))}
+//         <Route path="*" element={<Page404 />} />
+//       </Routes>
+//     </Suspense>
+//   );
+// };
+
+// export default Routings;
+
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import RequireAuth from '@/lib/components/auth/RequireAuth';
 import Page404 from '@/lib/pages/404';
+import Home from '@/lib/pages/home';
 
-import { routes, privateRoutes } from './routes';
+// Importez vos composants de page
+
 
 const Routings = () => {
   return (
     <Suspense>
       <Routes>
-        {routes.map((routeProps) => (
-          <Route {...routeProps} key={routeProps.path as string} />
-        ))}
-        {privateRoutes.map(({ element, ...privateRouteProps }) => (
-          <Route
-            element={
-              <RequireAuth
-                redirectTo={`/login?redirectTo=${privateRouteProps.path}`}
-              >
-                {element}
-              </RequireAuth>
-            }
-            {...privateRouteProps}
-            key={`privateRoute-${privateRouteProps.path}`}
-          />
-        ))}
+        {/* Routes publiques */}
+        <Route path="/" element={<Home />} />
+
+        {/* Routes privées */}
+        <Route
+          path="/private"
+          element={
+            <RequireAuth redirectTo="/login">
+              {/* Composant de la page privée */}
+              <div>Contenu de la page privée</div>
+            </RequireAuth>
+          }
+        />
+
+        {/* Route 404 */}
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Suspense>
@@ -43,3 +81,4 @@ const Routings = () => {
 };
 
 export default Routings;
+
--- frontend/src/lib/router/routes.tsx
+++ frontend/src/lib/router/routes.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import type { PathRouteProps } from 'react-router-dom';
+// import React from 'react';
+// import type { PathRouteProps } from 'react-router-dom';
 
-const Home = React.lazy(() => import('@/lib/pages/home'));
+// const Home = React.lazy(() => import('@/lib/pages/home'));
 
-export const routes: Array<PathRouteProps> = [
-  {
-    path: '/',
-    element: <Home />,
-  },
-];
+// export const routes: Array<PathRouteProps> = [
+//   {
+//     path: '/',
+//     element: <Home />,
+//   },
+// ];
 
-export const privateRoutes: Array<PathRouteProps> = [];
+// export const privateRoutes: Array<PathRouteProps> = [];
+
+// Routes.tsx
+
+// Routes.tsx
+
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from '@/lib/layout';
+import Page404 from '@/lib/pages/404'; // Importez votre composant Page404
+import Home from '@/lib/pages/home';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Route index element={<Home />} />
+          </Layout>
+        }
+      />
+      {/* Ajoutez la route Page404 */}
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
