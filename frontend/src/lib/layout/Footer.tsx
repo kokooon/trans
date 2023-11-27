5e52121df6ff2f89dfd230279beff18072f32541
Jean Oberle <joberle@k0r4p2.42mulhouse.fr>
2023-11-27 17:38:47 +0100

    form

--- frontend/src/lib/layout/Footer.tsx
+++ frontend/src/lib/layout/Footer.tsx
@@ -3,10 +3,6 @@ const Footer = () => {
     <footer className="wrapper">
       <div className="flex">
         <p className="text-xs">
-          {new Date().getFullYear()} -{' '}
-          <a href="https://sznm.dev" target="_blank" rel="noopener noreferrer">
-            sznm.dev
-          </a>
         </p>
       </div>
     </footer>
--- frontend/src/lib/pages/home/index.tsx
+++ frontend/src/lib/pages/home/index.tsx
@@ -1,16 +1,13 @@
-import { Button } from '@/lib/components/ui/button';
+// import { Button } from '@/lib/components/ui/button';
+import { CardForm } from '@/lib/components/auth/CardForm';
 
-import CTASection from './components/CTASection';
-import SomeText from './components/SomeText';
+// import CTASection from './components/CTASection';
+// import SomeText from './components/SomeText';
 
 const Home = () => {
   return (
     <div className="flex min-h-[60vh] flex-col items-center justify-center gap-8 text-center">
-      <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        test
-        </Button>
-      <SomeText />
-      <CTASection />
+      <CardForm></CardForm>
     </div>
   );
 };
