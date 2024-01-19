aa6bbaf1758e6232e40bae3486e4b6a6f63d2868
poseident <bschweit@DESKTOP-N4NMERS>
2024-01-19 12:42:12 +0100

    fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+//import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // fonts
@@ -11,7 +11,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
