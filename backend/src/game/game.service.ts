95d24cdb099e59c8d3fa25c5b4c7a728bfa30081
gaetan <gaetan.marzullo@yahoo.com>
2024-02-07 17:27:28 +0100

    jeu_v0.4

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserService } from 'src/user/user.service';
 import { Game } from 'src/entities/game.entity';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class GameService {
@@ -10,8 +11,46 @@ export class GameService {
     private userService: UserService,
     @InjectRepository(Game)
     private readonly GameRepository: Repository<Game>,
+    
     // ... other dependencies
   ) {}
 
+  async createGame(userAId: number, userBId: number): Promise<Game> {
+    // Créez un nouveau jeu
+    const game = new Game();
+    game.userA = userAId;
+    game.userB = userBId;
+    game.scoreA = 0;
+    game.scoreB = 0;
+    
+    // Enregistrez le jeu dans la base de données
+    const newGame = await this.GameRepository.save(game);
+
+    // Mettez à jour l'entité User pour chaque utilisateur avec l'ID de la partie
+    await this.updateUserGameHistory(userAId, newGame.id);
+    await this.updateUserGameHistory(userBId, newGame.id);
+
+    return newGame;
+  }
+
+  async updateUserGameHistory(userId: number, gameId: number): Promise<void> {
+    // Utilisez le service UserService pour obtenir l'utilisateur
+    const user = await this.userService.findById(userId);
+
+    // Vérifiez si l'utilisateur existe
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    // Ajoutez l'ID de la partie à l'historique des jeux de l'utilisateur
+    if (!user.History) {
+      user.History = [String(gameId)];
+    } else {
+      user.History.push(String(gameId));
+    }
+
+    // Enregistrez les modifications dans la base de données
+    await this.userService.save(user);
+  }
 
 }
