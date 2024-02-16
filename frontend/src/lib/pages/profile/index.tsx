be75bd505ee54648c88c9bc34636b336d8eb2141
Jean Oberle <joberle@k2r1p7.42mulhouse.fr>
2024-02-16 17:43:04 +0100

    profil

--- frontend/src/lib/pages/profile/index.tsx
+++ frontend/src/lib/pages/profile/index.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState} from 'react';
 import { UserNav } from '@/lib/components/ui/user-nav';
-import { fetchUserDetails, isTokenValid, isUserConnected } from '@/lib/components/utils/UtilsFetch';
-import { useNavigate } from 'react-router-dom';
+import { fetchUserDetails, fetchUserDetailsByPseudo, isTokenValid, isUserConnected } from '@/lib/components/utils/UtilsFetch';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Profile = () => {
     const navigate = useNavigate();
+    const [user, setUser] = useState<any | null>(null);
+    const { pseudo } = useParams();
 
     useEffect(() => {
         const checkToken = async () => {
@@ -16,6 +18,13 @@ const Profile = () => {
                 }
 
                 const userData = await fetchUserDetails();
+                console.log(pseudo);
+                if (pseudo !== null && pseudo !== undefined){
+                    console.log(pseudo);
+                    const profilData = await fetchUserDetailsByPseudo(pseudo);
+                    if (profilData)
+                        setUser(profilData);
+                }
 
                 if (userData[0].is2FAEnabled !== false) {
                     const isConnected = await isUserConnected();
@@ -33,8 +42,27 @@ const Profile = () => {
     }, [navigate]);
 
     return (
-        <div>
-            <UserNav />
+        <div className="bg-gradient-to-r from-amber-500 to-pink-500 min-h-screen flex flex-col">
+        <UserNav />
+        <div className="bg-gradient-to-r from-amber-500 to-pink-500 bg-[#292240] flex items-center justify-center min-h-screen text-[#9e9cb6]">
+    <section className="w-full max-w-[430px] relative bg-[#231f39]/60 rounded-[6px] shadow-[0px_15px_39px_16px_rgba(52,45,91,0.65)] backdrop-blur-sm mx-2 overflow-hidden">
+        <a href="" target="_blank" className="">
+            <img src={user && user.avatar ? user.avatar || 'placeholder_url' : 'placeholder_url'} className="rounded-full w-[120px] mx-auto my-10 p-0 border-[6px] box-content border-[#231f39] shadow-[0px_27px_16px_-11px_rgba(31,27,56,0.25)] transition-all duration-150 ease-in hover:scale-105 cursor-pointer slide-in-elliptic-top-fwd" />
+        </a>
+        <h1 className="text-xl font-bold text-center">{user && user.pseudo ? user.pseudo : 'Unknown User'}</h1>
+        <small className="block my-1 text-center">Level 42</small>
+        <p className="mt-5 text-center">User Interface design and front<br /> end developer.</p>
+        <div className="flex items-center justify-center gap-2 w-[80%] mx-auto mt-5 mb-10">
+            <button className="flex-1 border border-[#231f39] rounded-[4px] py-3 text-white bg-[#231f39] transition-all duration-150 ease-in hover:bg-[#472e99]">Add Friend</button>
+            <button className="flex-1 border border-[#231f39] text-[#ffffff] rounded-[4px] py-3 transition-all duration-150 ease-in hover:bg-[#472e99]  hover:text-white">Invite Game</button>
+        </div>
+        <div className="bg-[#1e1a36]/70 p-4 text-sm font-semibold backdrop-blur-sm">
+            <p>Stats</p>
+            <ul className="flex mt-4 flex-wrap items-center justify-start gap-2 gap-y-3">
+            </ul>
+        </div>
+    </section>
+</div>
         </div>
     );
 }
