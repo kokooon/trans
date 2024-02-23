e177947fb29cd29cc70188b0b45ef29ed9f1aa44
Gaetan Marzullo <gmarzull@k2r1p8.42mulhouse.fr>
2024-02-23 14:55:39 +0100

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { UserProvider } from './lib/components/utils/UserContext';
 import Layout from '@/lib/layout';
 import Routings from '@/lib/router/Routings';
+
 import { SocketProvider } from './lib/components/utils/socketContext';
 
 const App = () => (
